c48df88b8ff24bc8718c766b5a9d976249fd9e9f
Hrishikesh <hrishipuligal@gmail.com>
2025-08-22 15:38:37 +0530

    Changed UI  separated the style and script files for ease of access

--- scripts/UI.py
+++ scripts/UI.py
@@ -4,12 +4,12 @@ from scripts.load_music import (
 )  # assumes this function exists and works
 import cv2
 from openai import OpenAI
-from os import getenv
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 shared_data = None  # global reference to shared_data or detector passed from main
-api_key = getenv("OPENAI_API_KEY")
+api_key: str = str(load_dotenv(".env")["OPENAI_API_KEY"])
 
 
 def set_shared_data(data):
